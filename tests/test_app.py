34ebfd07a19c2bc1e423b90bf5d7405aed5784bc
RAGHUL <raghulmraghul981@gmail.com>
2026-05-13 17:59:35 +0530

    Single page Application

--- tests/test_app.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-
-
-def test_root():
-    app = create_app()
-    client = app.test_client()
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert b"Latest Posts" in resp.data
